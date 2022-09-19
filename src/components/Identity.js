cb7b9ba915d08f8fb39bb2a92554e631e4f11d4a
Nico Krause <inspiraluna@users.noreply.github.com>
2022-09-19 13:58:08 +0500

    changed fontsize of identity

--- src/components/Identity.js
+++ src/components/Identity.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { observer } from 'mobx-react'
-
+import { Box } from '@chakra-ui/react'
 const Identity = (props) => {
     console.log("Identity feed",props.store?.feed)
     return (
-        <div>
+        <Box fontSize='xs'>
             <div> My Identity: {props.store?.identity?.id} </div>
             <div> Odb Identity Id: {props.store?.odb?.identity.id} </div>
             <div> Odb Id: {props.store?.odb?.id} </div>
             <div> dbName: {props.store?.dbName} </div>
             <div> dbAddress: {props.store?.feed?.id} </div>
-        </div>
+          </Box>
         )
 }
 export default observer(Identity)
